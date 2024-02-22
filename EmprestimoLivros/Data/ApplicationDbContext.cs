58c4bedbaaccb36ccafaa9aeda6fad60f7457a2e
nandodevs <nando.devs@gmail.com>
2024-02-22 15:18:34 -0300

    Update project

--- EmprestimoLivros/Data/ApplicationDbContext.cs
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmprestimoLivros.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Classe que cria as tabelas do banco de dados
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
 
     }
 }
