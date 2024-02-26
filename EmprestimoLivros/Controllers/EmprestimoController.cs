861c01224dfa224ac76409c4fabb594f1c43c9f0
Nando Jr <nando.devs@gmail.com>
2024-02-25 21:00:47 -0300

    Projeto concluído

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -4,14 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EmprestimoLivros.Controllers
 {
-    public class EmprestimoController : Controller
+    public class EmprestimoController(ApplicationDbContext db) : Controller
     {
-        readonly private ApplicationDbContext _db;
-
-        public EmprestimoController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
+        readonly private ApplicationDbContext _db = db;
 
         public IActionResult Index()
         {
@@ -20,12 +15,112 @@ namespace EmprestimoLivros.Controllers
             return View(emprestimos);
         }
 
-        public IActionResult Cadastrar()
+        [HttpGet]
+        public IActionResult Excluir(int? id)
         {
+            try
+            {
+                if (id == null || id == 0)
+                {
+                    return NotFound("O ID não foi fornecido ou é inválido.");
+                }
 
-            return View();
+                EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+                if (emprestimo == null)
+                {
+                    return NotFound("Nenhum empréstimo encontrado com o ID fornecido.");
+                }
+
+                return View(emprestimo);
+            }
+            catch (Exception ex)
+            {
+                // Registre ou manipule a exceção conforme necessário
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+    
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            try
+            {
+                if (id == null || id == 0)
+                {
+                    return NotFound("O ID não foi fornecido ou é inválido.");
+                }
+
+                EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+                if (emprestimo == null)
+                {
+                    return NotFound("Nenhum empréstimo encontrado com o ID fornecido.");
+                }
+
+                return View(emprestimo);
+            }
+            catch (Exception ex)
+            {
+                // Registre ou manipule a exceção conforme necessário
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
         }
 
 
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                //Entra no BD -> Tabela Emprestimos -> Insere os dados
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges(); //Salva os dados
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+
+                return RedirectToAction("Index"); //Retorno para a tela Index de Empréstimos
+            }
+            return View(emprestimos);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Edição realizada com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["MensagemErro"] = "Erro ao editar o empréstimo!";
+            return View(emprestimo);
+              
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            TempData["MensagemSucesso"] = "Exclusão realizada com sucesso!";
+            return RedirectToAction("Index");
+        }
     }
 }
--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+newFunction();
+
+function newFunction() {
+    $(document).ready(function() {
+        $('#Emprestimos').DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "No data available in table",
+                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
+                "infoEmpty": "Showing 0 to 0 of 0 entries",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Show _MENU_ entries",
+                "loadingRecords": "Loading...",
+                "processing": "",
+                "search": "Procurar:",
+                "zeroRecords": "No matching records found",
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Último",
+                    "next": "Próximo",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "orderable": "Order by this column",
+                    "orderableReverse": "Reverse order this column"
+                }
+            }
+        });
+        setTimeout(function() {
+            $(".alert").fadeOut("slow", function() {
+                $(this).alert("close");
+            });
+        }, 5000);
+    });
+}
